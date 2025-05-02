54ed6594d3c0850163338e7a0fbbfeed1172f748
kamlakumari <125375477+kamlakumari@users.noreply.github.com>
2025-05-02 09:50:38 +0530

    Time: 3 ms (72.50%) | Memory: 14.9 MB (36.28%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
  public:
   vector<int> twoSum(vector<int>& nums, int target) {
-            unordered_map<int, int> numMap; // number -> index
+            unordered_map<int, int> numMap; 
 
         for (int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
