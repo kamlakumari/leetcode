ef84a800cb9141e624627d1c6f5f8e4a3d1ebc4e
kamlakumari <125375477+kamlakumari@users.noreply.github.com>
2025-05-11 22:37:03 +0530

    Time: 262 ms (32.82%) | Memory: 231.9 MB (38.72%) - LeetSync

---
+++ 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int longestEqualSubarray(vector<int>& nums, int k) {
+      unordered_map<int, vector<int>> positions;
+    
+    // Store the indices of each number
+    for (int i = 0; i < nums.size(); ++i) {
+        positions[nums[i]].push_back(i);
+    }
+
+    int maxLen = 0;
+
+
+    for (auto& [num, idxs] : positions) {
+        int left = 0;
+
+        for (int right = 0; right < idxs.size(); ++right) {
+            // Number of deletions required to make subarray equal
+            while ((idxs[right] - idxs[left] + 1) - (right - left + 1) > k) {
+                left++;
+            }
+            maxLen = max(maxLen, right - left + 1);
+        }
+    }
+
+    return maxLen;
+}
+
+int main() {
+    vector<int> nums = {1, 3, 2, 3, 1, 3};
+    int k = 3;
+    cout << "Output: " << longestEqualSubarray(nums, k) << endl;
+    return 0;
+}
+};
