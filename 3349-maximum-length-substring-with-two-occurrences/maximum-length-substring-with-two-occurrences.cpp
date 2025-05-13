1b2772eeadd678f15f001a051326e02530befdf8
kamlakumari <125375477+kamlakumari@users.noreply.github.com>
2025-05-13 11:15:09 +0530

    Time: 3 ms (48.27%) | Memory: 9.1 MB (84.52%) - LeetSync

---
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+      std::vector<int> count(26, 0); 
+    int maxLength = 0;
+    int left = 0;
+
+    for (int right = 0; right < s.length(); ++right) {
+        count[s[right] - 'a']++;
+
+        // If any character occurs more than twice, move the left pointer
+        while (count[s[right] - 'a'] > 2) {
+            count[s[left] - 'a']--;
+            left++;
+        }
+
+        maxLength = std::max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+}
+};
